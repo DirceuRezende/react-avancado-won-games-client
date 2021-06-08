0e77e2b1e76a8469f4f8fb2e12b0f81c21a449ee
Dirceu Rezende <dirceu.srezende@gmail.com>
2021-06-08 18:46:16 -0300

    Alterações no componente Highlight

--- src/components/Highlight/index.tsx
+++ src/components/Highlight/index.tsx
@@ -1,8 +1,29 @@
+import Button from 'components/Button'
 import * as S from './styles'
 
-const Highlight = () => (
-  <S.Wrapper>
-    <h1>Highlight</h1>
+export type HightlightProps = {
+  title: string
+  subtitle: string
+  backgroundImage: string
+  buttonLabel: string
+  buttonLink: string
+}
+
+const Highlight = ({
+  title,
+  subtitle,
+  backgroundImage,
+  buttonLabel,
+  buttonLink
+}: HightlightProps) => (
+  <S.Wrapper backgroundImage={backgroundImage}>
+    <S.Content>
+      <S.Title>{title}</S.Title>
+      <S.Subtitle>{subtitle}</S.Subtitle>
+      <Button as="a" href={buttonLink}>
+        {buttonLabel}
+      </Button>
+    </S.Content>
   </S.Wrapper>
 )
 
