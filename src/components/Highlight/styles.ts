87dfcdfc290a8fdd5ff35cb40c9e148c34cb2b94
Dirceu Rezende <dirceu.srezende@gmail.com>
2021-06-10 19:17:09 -0300

    Finalizado o componente Highlight

--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -2,10 +2,33 @@ import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 import { HightlightProps } from '.'
 
-type WrapperProps = Pick<HightlightProps, 'backgroundImage'>
+type WrapperProps = Pick<HightlightProps, 'backgroundImage' | 'alignment'>
+
+const wrapperModifiers = {
+  right: () => css`
+    grid-template-areas: 'floatimage content';
+    grid-template-columns: 1.3fr 2fr;
+
+    ${Content} {
+      text-align: right;
+    }
+  `,
+  left: () => css`
+    grid-template-areas: 'content floatimage';
+    grid-template-columns: 2fr 1.3fr;
+
+    ${Content} {
+      text-align: left;
+    }
+
+    ${FloatImage} {
+      justify-self: end;
+    }
+  `
+}
 
 export const Wrapper = styled.section<WrapperProps>`
-  ${({ backgroundImage }) => css`
+  ${({ alignment, backgroundImage }) => css`
     background-image: url(${backgroundImage});
     background-position: center center;
     background-size: cover;
@@ -20,17 +43,32 @@ export const Wrapper = styled.section<WrapperProps>`
       height: 100%;
       background-color: rgba(0, 0, 0, 0.6);
     }
-  `}
-
-  ${media.greaterThan('medium')`
+    ${media.greaterThan('medium')`
     height: 32rem;
+    `}
+
+    ${wrapperModifiers[alignment!]()}
+  `}
+`
+
+export const FloatImage = styled.img`
+  ${({ theme }) => css`
+    align-self: end;
+    grid-area: floatimage;
+    z-index: ${theme.layers.base};
+    max-height: 23rem;
+    max-width: 100%;
+
+    ${media.greaterThan('medium')`
+      height: 32rem;
+    `}
   `}
 `
 
 export const Content = styled.div`
   ${({ theme }) => css`
+    grid-area: content;
     z-index: ${theme.layers.base};
-    text-align: right;
     padding: ${theme.spacings.xsmall};
 
     ${media.greaterThan('medium')`
