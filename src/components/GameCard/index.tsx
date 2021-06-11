5f263ffce2fb9651716ac749159f7e6aa27d135d
Dirceu Rezende <dirceu.srezende@gmail.com>
2021-06-11 20:28:34 -0300

    Adicionado testes, estrutura e estilos para o componente GameCard

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,8 +1,44 @@
-import * as S from './styles'
+import {
+  FavoriteBorder,
+  AddShoppingCart
+} from '@styled-icons/material-outlined'
 
-const GameCard = () => (
+import * as S from './styles'
+import Button from 'components/Button'
+
+export type GameCardProps = {
+  title: string
+  developer: string
+  img: string
+  price: string
+  promotionalPrice?: string
+}
+
+const GameCard = ({
+  title,
+  developer,
+  img,
+  price,
+  promotionalPrice
+}: GameCardProps) => (
   <S.Wrapper>
-    <h1>GameCard</h1>
+    <S.ImageBox>
+      <img src={img} alt={title} />
+    </S.ImageBox>
+    <S.Content>
+      <S.Info>
+        <S.Title>{title}</S.Title>
+        <S.Developer>{developer}</S.Developer>
+      </S.Info>
+      <S.FavButton role="button">
+        <FavoriteBorder aria-label="Add to Wishlist" />
+      </S.FavButton>
+      <S.BuyBox>
+        {!!promotionalPrice && <S.Price isPromotional>{price}</S.Price>}
+        <S.Price>{promotionalPrice || price}</S.Price>
+        <Button icon={<AddShoppingCart />} size="small" />
+      </S.BuyBox>
+    </S.Content>
   </S.Wrapper>
 )
 
--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -1,15 +1,59 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameCard from '.'
 
+const props = {
+  title: 'Population Zero',
+  developer: 'Rockstar Games',
+  img: 'https://source.unsplash.com/user/willianjusten/300x140',
+  price: 'R$ 200,00'
+}
+
 describe('<GameCard />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<GameCard />)
+  it('should render correctly', () => {
+    renderWithTheme(<GameCard {...props} />)
 
     expect(
-      screen.getByRole('heading', { name: /GameCard/i })
+      screen.getByRole('heading', { name: props.title })
     ).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('heading', { name: props.developer })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
+
+    expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument()
+  })
+
+  it('should render price in label', () => {
+    // renderiza o componente
+    renderWithTheme(<GameCard {...props} />)
+    // preço não tenha line-through
+    expect(screen.getByText(props.price)).not.toHaveStyle({
+      'text-decoration': 'line-through'
+    })
+    // preço tenha o background secundário
+    expect(screen.getByText(props.price)).toHaveStyle({
+      'background-color': '#3CD3C1'
+    })
+  })
+
+  it('should render a line-through in price when promotional', () => {
+    // renderiza o componente (COM promotionalPrice) // 200 reais // 15 reais
+    const promotionalPrice = 'R$ 15,00'
+    renderWithTheme(<GameCard {...props} promotionalPrice={promotionalPrice} />)
+    // preço tenha line-through (200)
+    expect(screen.getByText(props.price)).toHaveStyle({
+      'text-decoration': 'line-through'
+    })
+    // preço novo promocional não vai ter line-through (15)
+    expect(screen.getByText(promotionalPrice)).not.toHaveStyle({
+      'text-decoration': 'line-through'
+    })
   })
 })
