36bb11ac9c5bba1613bc80a2eae916adee6b3133
Dirceu Rezende <dirceu.srezende@gmail.com>
2021-08-06 19:38:15 -0300

    Add Base Template and refactor Home

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,11 +1,11 @@
+import Base from 'templates/Base'
+
 import { BannerProps } from 'components/Banner'
-import BannerSlider from 'components/BannerSlider'
 import { GameCardProps } from 'components/GameCard'
 import { HighlightProps } from 'components/Highlight'
 import Showcase from 'components/Showcase'
+import BannerSlider from 'components/BannerSlider'
 import { Container } from 'components/Container'
-import Footer from 'components/Footer'
-import Menu from 'components/Menu'
 
 import * as S from './styles'
 
@@ -32,9 +32,8 @@ const Home = ({
   freeGames,
   freeHighligth
 }: HomeTemplateProps) => (
-  <section>
+  <Base>
     <Container>
-      <Menu />
       <S.SectionBanner>
         <BannerSlider items={banners} />
       </S.SectionBanner>
@@ -56,13 +55,7 @@ const Home = ({
     </S.SectionUpcoming>
 
     <Showcase title="Free Games" highlight={freeHighligth} games={freeGames} />
-
-    <S.SectionFooter>
-      <Container>
-        <Footer />
-      </Container>
-    </S.SectionFooter>
-  </section>
+  </Base>
 )
 
 export default Home
