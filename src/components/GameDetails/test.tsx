1f0947ffaa09e6d76bfe8391e1d397337ac7ff25
Dirceu Rezende <dirceu.srezende@gmail.com>
2021-08-09 19:55:27 -0300

    Add GameDetails component

---
+++ src/components/GameDetails/test.tsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react'
+//import { renderWithTheme } from 'utils/tests/helpers'
+
+//import GameDetails from '.'
+
+describe('<GameDetails />', () => {
+  it('should render the heading', () => {
+    render(<div></div>)
+    expect(0).toBe(0)
+  })
+})
