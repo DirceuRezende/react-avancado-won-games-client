382bc36cdad463bc259052cbaffba505d093bb17
Dirceu Rezende <dirceu.srezende@gmail.com>
2021-08-10 19:22:49 -0300

    Add GameDetails Test

---
+++ src/components/GameDetails/mock.ts
@@ -0,0 +1,8 @@
+export default {
+  developer: 'Different Tales',
+  releaseDate: '2021-08-10T23:00:00',
+  platforms: ['windows', 'mac', 'linux'],
+  publisher: 'Walkabout',
+  rating: 'BR0',
+  genres: ['Role-playing']
+}
