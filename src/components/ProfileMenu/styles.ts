685fcd340df8c0fd9781bbdbdef702f1a9d28501
Dirceu Rezende <dirceu.srezende@gmail.com>
2021-08-21 12:07:43 -0300

    Add activeLink to ProfileMenu component

--- src/components/ProfileMenu/styles.ts
+++ src/components/ProfileMenu/styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components'
+import styled, { css, DefaultTheme } from 'styled-components'
 import media from 'styled-media-query'
 
 export const Nav = styled.nav`
@@ -7,19 +7,33 @@ export const Nav = styled.nav`
     border-bottom: 0.1rem solid ${theme.colors.lightGray};
 
     ${media.greaterThan('medium')`
-flex-direction: column;
-border: 0;
-a:not(:last-child) {
-  border-bottom: 0.1rem solid ${theme.colors.lightGray};
-}
-`}
+      flex-direction: column;
+      border: 0;
+      a:not(:last-child) {
+        border-bottom: 0.1rem solid ${theme.colors.lightGray};
+      }
+    `}
   `}
 `
 
-export const Link = styled.a`
-  ${({ theme }) => css`
+const linkModifiers = {
+  default: (theme: DefaultTheme) => css`
     background: ${theme.colors.white};
     color: ${theme.colors.black};
+  `,
+
+  active: (theme: DefaultTheme) => css`
+    background: ${theme.colors.primary};
+    color: ${theme.colors.white};
+  `
+}
+
+type LinkProps = {
+  isActive?: boolean
+}
+
+export const Link = styled.a<LinkProps>`
+  ${({ theme, isActive }) => css`
     text-decoration: none;
     display: flex;
     align-items: center;
@@ -33,11 +47,13 @@ export const Link = styled.a`
       margin-left: ${theme.spacings.xsmall};
     }
     ${media.lessThan('medium')`
-justify-content: center;
-flex: 1;
-> span {
-  display: none;
-}
-`}
+      justify-content: center;
+      flex: 1;
+      > span {
+        display: none;
+      }
+    `}
+    ${!isActive && linkModifiers.default(theme)};
+    ${isActive && linkModifiers.active(theme)};
   `}
 `
