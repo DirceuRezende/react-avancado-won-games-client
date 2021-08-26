f18d0bd899b78c230081e00d89bceede45e203df
Dirceu Rezende <dirceu.srezende@gmail.com>
2021-08-26 19:51:07 -0300

    Add graphql query QUERY_GAMES on Games page

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,15 +1,30 @@
 import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
 import filterItemsMock from 'components/ExploreSidebar/mock'
-import gamesMock from 'components/GameCardSlider/mock'
+import { QUERY_GAMES } from 'graphql/queries/games'
+import { initializeApollo } from 'utils/apollo'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
 }
 
 export async function getServerSideProps() {
+  const apolloClient = initializeApollo()
+  const { data } = await apolloClient.query({
+    query: QUERY_GAMES,
+    variables: { limit: 9 }
+  })
   return {
     props: {
-      games: gamesMock,
+      revalidate: 60,
+      games: data.games.map((game) => ({
+        title: game.name,
+        developer: game.developers[0].name,
+        img: `http://localhost:1337${game.cover.url}`,
+        price: new Intl.NumberFormat('en', {
+          style: 'currency',
+          currency: 'USD'
+        }).format(game.price)
+      })),
       filterItems: filterItemsMock
     }
   }
