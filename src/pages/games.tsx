32d0cd5ab0742aad0843024d7100bce4ca849c04
Dirceu Rezende <dirceu.srezende@gmail.com>
2021-08-27 18:54:10 -0300

    Add link to GameCard and update tests, stories and mocks

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -2,6 +2,7 @@ import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
 import filterItemsMock from 'components/ExploreSidebar/mock'
 import { QUERY_GAMES } from 'graphql/queries/games'
 import { initializeApollo } from 'utils/apollo'
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
@@ -9,7 +10,7 @@ export default function GamesPage(props: GamesTemplateProps) {
 
 export async function getServerSideProps() {
   const apolloClient = initializeApollo()
-  const { data } = await apolloClient.query({
+  const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({
     query: QUERY_GAMES,
     variables: { limit: 9 }
   })
@@ -17,9 +18,10 @@ export async function getServerSideProps() {
     props: {
       revalidate: 60,
       games: data.games.map((game) => ({
+        slug: game.slug,
         title: game.name,
         developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover.url}`,
+        img: `http://localhost:1337${game.cover!.url}`,
         price: new Intl.NumberFormat('en', {
           style: 'currency',
           currency: 'USD'
