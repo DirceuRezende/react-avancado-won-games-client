8012e2ff9f84120802a7a5b39550d3e747aa5794
Dirceu Rezende <dirceu.srezende@gmail.com>
2021-09-11 12:23:22 -0300

    Remove default autofill from browser

--- src/components/TextField/styles.ts
+++ src/components/TextField/styles.ts
@@ -1,11 +1,15 @@
 import styled, { css, DefaultTheme } from 'styled-components'
+
 import { TextFieldProps } from '.'
 
 type IconPositionProps = Pick<TextFieldProps, 'iconPosition'>
 
+type WrapperProps = Pick<TextFieldProps, 'disabled'> & { error?: boolean }
+
 export const InputWrapper = styled.div`
   ${({ theme }) => css`
     display: flex;
+    align-items: center;
     background: ${theme.colors.lightGray};
     border-radius: 0.2rem;
     padding: 0 ${theme.spacings.xsmall};
@@ -27,7 +31,12 @@ export const Input = styled.input<IconPositionProps>`
     background: transparent;
     border: 0;
     outline: none;
-    width: 100%;
+    width: ${iconPosition === 'right' ? `calc(100% - 2.2rem)` : `100%`};
+    &:-webkit-autofill {
+      -webkit-box-shadow: 0 0 0 ${theme.spacings.small}
+        ${theme.colors.lightGray} inset;
+      filter: none
+    }
   `}
 `
 
@@ -42,11 +51,11 @@ export const Label = styled.label`
 export const Icon = styled.div<IconPositionProps>`
   ${({ theme, iconPosition }) => css`
     display: flex;
-    width: 2.2rem;
     color: ${theme.colors.gray};
     order: ${iconPosition === 'right' ? 1 : 0};
     & > svg {
-      width: 100%;
+      width: 2.2rem;
+      height: 100%;
     }
   `}
 `
@@ -58,9 +67,16 @@ export const Error = styled.p`
   `}
 `
 
-type WrapperProps = Pick<TextFieldProps, 'disabled'> & { error?: boolean }
-
 const wrapperModifiers = {
+  error: (theme: DefaultTheme) => css`
+    ${InputWrapper} {
+      border-color: ${theme.colors.red};
+    }
+    ${Icon},
+    ${Label} {
+      color: ${theme.colors.red};
+    }
+  `,
   disabled: (theme: DefaultTheme) => css`
     ${Label},
     ${Input},
@@ -71,20 +87,11 @@ const wrapperModifiers = {
         color: currentColor;
       }
     }
-  `,
-  error: (theme: DefaultTheme) => css`
-    ${InputWrapper} {
-      border-color: ${theme.colors.red};
-    }
-    ${Icon},
-    ${Label} {
-      color: ${theme.colors.red};
-    }
   `
 }
 
 export const Wrapper = styled.div<WrapperProps>`
-  ${({ theme, disabled, error }) => css`
+  ${({ theme, error, disabled }) => css`
     ${error && wrapperModifiers.error(theme)}
     ${disabled && wrapperModifiers.disabled(theme)}
   `}
