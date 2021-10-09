f5c49558fda2ffc438870f04b11167a4adf7e59a
Dirceu Rezende <dirceu.srezende@gmail.com>
2021-10-09 17:31:31 -0300

    Add e2e tests on Sign Up page

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -26,6 +26,7 @@
 
 // Add Testing Library Commands
 import '@testing-library/cypress/add-commands';
+import { User } from './generate';
 
 Cypress.Commands.add('google', () => cy.visit('https://google.com'))
 
@@ -92,4 +93,12 @@ Cypress.Commands.add('shouldBeLessThan', (value) => {
     .then($el => $el.replace('$', ''))
     .then(parseFloat)
     .should('be.lt', value)
+})
+
+Cypress.Commands.add('signUp', (user: User) => {
+  cy.findByPlaceholderText(/username/i).type(user.username)
+  cy.findByPlaceholderText(/email/i).type(user.email)
+  cy.findByPlaceholderText(/^password/i).type(user.password)
+  cy.findByPlaceholderText(/confirm password/i).type(user.password)
+  cy.findByRole('button', { name: /sign up now/i }).click()
 })
