3c59b14cebeda523fbc0e1f3927a1a22bc03f7b2
Dirceu Rezende <dirceu.srezende@gmail.com>
2021-10-12 12:47:33 -0300

    Update getImageUrl

--- src/utils/getImageUrl.ts
+++ src/utils/getImageUrl.ts
@@ -1,5 +1,5 @@
 export const getImageUrl = (url: string | undefined) => {
-  if (process.env.NEXT_PUBLIC_IMAGE_HOST) {
+  if (process.env.NEXT_PUBLIC_IMAGE_HOST && url && url[0] === '/') {
     return `${process.env.NEXT_PUBLIC_IMAGE_HOST}${url}`
   }
 
